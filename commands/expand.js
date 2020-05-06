a7cab668a62faa215b65ad1bdb01d9efa66ad18c
Adrian Gose <adrian@gose.pw>
2020-05-05 21:04:46 -0500

    Add expand command
    The expand command takes in a message, and adds spaces to it and sends it back to the current channel.
    
    The expand command defaults to 1 space, however, a number of spaces can be specified.
    
    Example usage:
    
    !expand test -> 't e s t'
    
    !expand 2 test -> 't  e  s  t'

---
+++ commands/expand.js
@@ -0,0 +1,25 @@
+const expand = require('expand-string-professionally')
+
+module.exports = {
+    name: 'expand',
+    description: 'For when a string must be expanded',
+    execute(message, args, newEmbed) {
+        if (args.length === 0)
+            return message.reply(`You didn't give me a string to expand, asshole`);
+
+        let numSpacing = args[0]
+        let messageToProcess = ''
+
+        if (Number.isInteger(numSpacing * 1)) {
+            // Clamp to max value of 16 bit integer, will fix 
+            // expand-string-professionally to handle this later, in 10 years probably
+            messageToProcess = expand(args.splice(1).join(' '), numSpacing > 65536 ? 65536 : numSpacing)
+        } else {
+            messageToProcess = expand(args.join(' '))
+        }
+
+        // Truncate to ensure we do not exceed discord 2000 char limit
+        let truncated = messageToProcess.substring(0, Math.min(2000, messageToProcess.length))
+        return message.channel.send(truncated);
+    },
+};
