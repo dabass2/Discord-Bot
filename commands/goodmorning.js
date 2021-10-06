df9f9d5cea4c2324a75c955b9a3690d4611ff996
Daniel Bass <danielbass123456@gmail.com>
2021-10-05 21:26:20 -0500

    A little spice

--- commands/goodmorning.js
+++ commands/goodmorning.js
@@ -2,10 +2,20 @@ module.exports = {
   name: 'goodmorning',
   description: 'Wishes a homie goodmorning : )',
   execute(message, args, newEmbed) {
-      newEmbed    
-      .setColor("#ffffff")
-      .setDescription('Goodmorning homie! : ) :sun_with_face:')
-      .setImage("https://i.imgur.com/u2IfXac.png")
-      return message.channel.send(newEmbed);
+      a = Math.floor(Math.random() * 11);
+      if (a >= 8) { // 20% chance
+          newEmbed
+          .setColor("#ffffff")
+          .setDescription('Good morning homie! : ) :sun_with_face:')
+          .setImage("https://c.tenor.com/NdzzTkkCvB4AAAAC/peepo-wiadro.gif")
+          return message.channel.send(newEmbed);
+      } else { // 80% chance
+          newEmbed    
+          .setColor("#ffffff")
+          .setDescription('Good morning homie! : ) :sun_with_face:')
+          .setImage("https://i.imgur.com/u2IfXac.png")
+          return message.channel.send(newEmbed);
+      }
+
   },
 };
--- commands/goodnight.js
+++ commands/goodnight.js
@@ -2,10 +2,21 @@ module.exports = {
     name: 'goodnight',
     description: 'Wishes a homie goodnight : )',
     execute(message, args, newEmbed) {
-        newEmbed
-        .setColor("#000000")
-        .setDescription('Goodnight homie! : ) :heart:')
-        .setImage("https://i.imgur.com/Hy0cpR2.png")
-        return message.channel.send(newEmbed);
+        a = Math.floor(Math.random() * 11);
+        if (a >= 8) { // 20% chance
+            newEmbed
+            .setColor("#000000")
+            .setDescription('Goodnight Daddy :tired_face:')
+            .setThumbnail("https://i.imgur.com/miGCppP.gif")
+            .setImage("https://i.imgur.com/miGCppP.gif")
+            .setAuthor(message.author.username, "https://i.imgur.com/miGCppP.gif")
+            return message.channel.send(newEmbed);
+        } else { // 80% chance
+            newEmbed
+            .setColor("#000000")
+            .setDescription('Goodnight homie! : ) :heart:')
+            .setImage("https://i.imgur.com/Hy0cpR2.png")
+            return message.channel.send(newEmbed);
+        }
     },
 };
