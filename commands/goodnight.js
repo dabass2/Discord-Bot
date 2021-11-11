ccfa287ce55094cf7314940d0df00fed38080987
Daniel Bass <danielbass123456@gmail.com>
2021-11-10 22:30:45 -0600

    updated goodnight

--- commands/goodnight.js
+++ commands/goodnight.js
@@ -3,6 +3,7 @@ module.exports = {
     description: 'Wishes a homie goodnight : )',
     execute(message, args, newEmbed) {
         a = Math.floor(Math.random() * 11);
+        console.log(a)
         if (a >= 8) { // 20% chance
             newEmbed
             .setColor("#000000")
@@ -11,13 +12,17 @@ module.exports = {
             .setImage("https://i.imgur.com/miGCppP.gif")
             .setAuthor(message.author.username, "https://i.imgur.com/miGCppP.gif")
             .setFooter("🥺", "https://i.imgur.com/miGCppP.gif")
-            return message.channel.send(newEmbed);
+        } else if (a == 3) {
+            newEmbed
+            .setColor("#000000")
+            .setDescription('Goodnight homie! : ) :heart:')
+            .setImage("https://c.tenor.com/6l7lYskHVDQAAAAC/baal-genshin-baal.gif")
         } else { // 80% chance
             newEmbed
             .setColor("#000000")
             .setDescription('Goodnight homie! : ) :heart:')
             .setImage("https://i.imgur.com/Hy0cpR2.png")
-            return message.channel.send(newEmbed);
         }
+        return message.channel.send(newEmbed);
     },
 };
