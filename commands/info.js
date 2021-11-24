d3a2b00a59c4fcdc4efd472aea6e90c1e336a95b
Daniel <danielbass123456@gmail.com>
2021-11-24 18:11:20 +0000

    Updated info.js

--- commands/info.js
+++ commands/info.js
@@ -2,17 +2,19 @@ module.exports = {
     name: 'info',
     description: 'Gives info about a user',
     execute(message, args, newEmbed) {
+      //console.log(message.author.avatarURL)
       if (args.length == 0)
       {
-        let usrPic = message.author.displayAvatarURL;
+        //let usrPic = message.author.avatarURL;
         newEmbed
         .setDescription('User Information')
         .setColor('#97e552')
-        .setThumbnail(usrPic)
+	.setImage(message.author.avatarURL)
+        //.setThumbnail(usrPic)
         .addField('Username', message.author.username)
         .addField('User Created on', message.author.createdAt)
-        .addField('Joined at', message.member.joinedAt)
-        .addField('Last message', message.createdAt)
+        //.addField('Joined at', message.member.joinedAt)
+        //.addField('Last message', message.)
         return message.channel.send(newEmbed);
       }
       
