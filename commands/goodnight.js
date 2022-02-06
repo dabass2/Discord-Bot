6aef9616dd719300dd33fd5ddfee73c85db60ee2
Daniel Bass <danielbass123456@gmail.com>
2022-02-06 10:40:55 -0600

    Updated goodnight.js

--- commands/goodnight.js
+++ commands/goodnight.js
@@ -6,21 +6,16 @@ module.exports = {
         .setName('goodnight')
         .setDescription('Wishes a homie goodnight : )'),
     async execute(interaction, msgEmbed) {
-        a = Math.floor(Math.random() * 11);
-
-        let img
-        if (a >= 8) {
-            await curse.execute(interaction, msgEmbed)
-            return
-        } else if (a == 3) {
-            img = "https://c.tenor.com/6l7lYskHVDQAAAAC/baal-genshin-baal.gif"
-        } else {
-            img = "https://i.imgur.com/Hy0cpR2.png"
+        let nights = ["https://c.tenor.com/6l7lYskHVDQAAAAC/baal-genshin-baal.gif", "https://i.imgur.com/Hy0cpR2.png", "https://c.tenor.com/pEykNQWTaKIAAAAC/pepe-pepo.gif", "curse"]
+        img = nights[Math.floor(Math.random() * nights.length)]
+        if (img === "curse") {
+          await curse.execute(interaction, msgEmbed)
+          return
         }
 
-        msgEmbed
+        msgEmbed    
             .setColor("#000000")
-            .setDescription('Goodnight homie! : ) :heart:')
+            .setDescription('Good night homie! : ) :new_moon_with_face:')
             .setImage(img)
         await interaction.reply({ embeds: [msgEmbed] })
     }
