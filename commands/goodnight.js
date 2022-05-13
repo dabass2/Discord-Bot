e5a83f2a00ce0d503b324cbd527f51cd7adf1d47
Daniel Bass <danielbass123456@gmail.com>
2022-05-12 20:31:18 -0500

    Add kermit

--- commands/goodnight.js
+++ commands/goodnight.js
@@ -7,7 +7,8 @@ module.exports = {
         .setDescription('Wishes a homie goodnight : )'),
     async execute(interaction, msgEmbed) {
         let nights = ["https://c.tenor.com/6l7lYskHVDQAAAAC/baal-genshin-baal.gif", "https://i.imgur.com/Hy0cpR2.png",
-                         "https://c.tenor.com/pEykNQWTaKIAAAAC/pepe-pepo.gif", "curse"]
+                         "https://c.tenor.com/pEykNQWTaKIAAAAC/pepe-pepo.gif", 
+                         "https://cdn.discordapp.com/attachments/110419059232780288/974151832433741884/original.jpg", "curse"]
         let img = nights[Math.floor(Math.random() * nights.length)]
         if (img === "curse") {
           await curse.execute(interaction, msgEmbed)
