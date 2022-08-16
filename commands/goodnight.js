4943b411ae780920241a7b06de4c562bd307b1bc
Daniel <danielbass123456@gmail.com>
2022-08-16 00:44:53 +0000

    update goodnight

--- commands/goodnight.js
+++ commands/goodnight.js
@@ -7,15 +7,16 @@ module.exports = {
         .setDescription('Wishes a homie goodnight : )'),
     async execute(interaction, msgEmbed) {
         let nights = ["https://c.tenor.com/6l7lYskHVDQAAAAC/baal-genshin-baal.gif", "https://i.imgur.com/Hy0cpR2.png",
-                         "https://c.tenor.com/pEykNQWTaKIAAAAC/pepe-pepo.gif", 
-                         "https://cdn.discordapp.com/attachments/110419059232780288/974151832433741884/original.jpg", "curse"]
+            "https://c.tenor.com/pEykNQWTaKIAAAAC/pepe-pepo.gif",
+            "https://cdn.discordapp.com/attachments/110419059232780288/974151832433741884/original.jpg",
+            "https://c.tenor.com/ZDF0F9Ehj0IAAAAC/hasanabi-hasan-piker.gif", "curse"]
         let img = nights[Math.floor(Math.random() * nights.length)]
         if (img === "curse") {
-          await curse.execute(interaction, msgEmbed)
-          return
+            await curse.execute(interaction, msgEmbed)
+            return
         }
 
-        msgEmbed    
+        msgEmbed
             .setColor("#000000")
             .setDescription('Good night homie! : ) :new_moon_with_face:')
             .setImage(img)
